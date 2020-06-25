de2161e05d87bb07e19f89b8db8caedc934ef48a
James Hedberg <jhedberg@ccny.cuny.edu>
2020-06-25 08:30:01 -0400

    comments

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
  Simulating the CMB for Visualization
 
  Using pixell and camb
+
+ Thanks Alex.
--- create_allsky_equi.py
+++ create_allsky_equi.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun 23 07:49:52 2020
+Created on Thur Jun 25 07:49:52 2020
 
 """
 
@@ -10,12 +10,17 @@ import matplotlib as mpl, matplotlib.pyplot as plt, numpy as np
 from pixell import enmap, curvedsky, utils
 from matplotlib.colors import ListedColormap
 
+# Planck ColorMap from zonca
 colombi1_cmap = ListedColormap(np.loadtxt("Planck_Parchment_RGB.txt")/255.)
 cmap = colombi1_cmap
+# for no border on saved image
 mpl.rcParams['savefig.pad_inches'] = 0
+# make seed the same
 np.random.seed(0)
 
- #Set up a new set of parameters for CAMB
+#####
+#From ajvanengelen
+#Set up a new set of parameters for CAMB
 pars = camb.CAMBparams()
 #This function sets up CosmoMC-like settings, with one massive neutrino and heliu m set using BBN consistency
 pars.set_cosmology(H0=67.5, ombh2=0.022, omch2=0.122, mnu=0.06, omk=0, tau=0.06)
@@ -25,21 +30,24 @@ pars.set_for_lmax(2500, lens_potential_accuracy=0);
 results = camb.get_results(pars)
 powers =results.get_cmb_power_spectra(pars, CMB_unit='muK')
 
-#D_ell.shape
+
 
 D_ell = powers['total'][:,0] 
+
 ells = np.arange(D_ell.shape[0])
-# plt.plot(D_ell)
-# plt.ylabel('$D_\ell$')
-# plt.xlabel('$\ell')
+
 
 C_ell = D_ell / ((ells * (ells + 1) / (2. * np.pi)))
 C_ell[0:2] = 0 # just set these to 0 to avoid division by 0
 
-box = np.array([[-10,10],[10,-10]]) * utils.degree
-#shape,wcs = enmap.geometry(pos=box,res=0.5 * utils.arcmin,proj='car')
-shape,wcs = enmap.fullsky_geometry(res=1 * utils.arcmin,proj='car')
+####
+
+
+#prepare to plot in equirectangular projection
+shape,wcs = enmap.fullsky_geometry(res=30 * utils.arcmin,proj='car')
 cmb_map = curvedsky.rand_map(shape, wcs, C_ell)
+
+#plot settings
 figsize = 20,10
 fig = plt.figure(figsize=figsize)
 ax = plt.axes([0,0,1,1], frameon=False)
